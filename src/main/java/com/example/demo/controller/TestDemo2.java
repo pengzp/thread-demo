bdbda4fa8969639d87c627afbfe3a6fd3a2a948d
pengzhipeng <1156715571@qq.com>
2020-08-11 17:28:25 +0800

    用于测试集合类的线程安全问题

---
+++ src/main/java/com/example/demo/controller/TestDemo2.java
@@ -0,0 +1,59 @@
+package com.example.demo.controller;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * @Author: 彭志鹏
+ * @DateTime: 2020/7/31 15:31
+ * @Description:
+ */
+public class TestDemo2 {
+
+    public static void main(String[] args) {
+
+        List list = new CopyOnWriteArrayList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Iterator<Integer> iterator = list.iterator();
+                while (iterator.hasNext()){
+                    Integer integer = iterator.next();
+                    System.out.println("当前值:"+integer);
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        },"A").start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Iterator<Integer> iterator = list.iterator();
+                while (iterator.hasNext()){
+                    Integer integer = iterator.next();
+                    if (integer == 2){
+                        //iterator.remove();
+                        list.remove(integer);
+                    }
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        },"B").start();
+    }
+}
---
+++ src/main/java/com/example/demo/controller/TestVector.java
@@ -0,0 +1,99 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Vector;
+
+/**
+ * @Author: 彭志鹏
+ * @DateTime: 2020/8/11 17:01
+ * @Description:
+ */
+public class TestVector {
+
+    public static void main(String[] args) {
+
+        /*List list = new Vector();
+        //混淆了同步修改异常和线程安全
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Iterator<Integer> iterator = list.iterator();
+                while (iterator.hasNext()){
+                    Integer integer = iterator.next();
+                    System.out.println("当前值:"+integer);
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        },"A").start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Iterator<Integer> iterator = list.iterator();
+                while (iterator.hasNext()){
+                    Integer integer = iterator.next();
+                    if (integer == 2){
+                        //iterator.remove();
+                        list.remove(integer);
+                    }
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        },"B").start();*/
+
+        TestVector tt = new TestVector();
+        tt.train();
+    }
+
+    /**
+     * 基本上所有的集合类都有一个叫做快速失败（Fail-Fast)的校验机制，当一个集合在被多个线程修改并访问时，
+     * 就可能会出现 ConcurrentModificationException异常，
+     * 这是为了确保集合方法一致而设置的保护措施
+     */
+
+    public void train(){
+        //火车票列表
+        final List<String> tickets = new Vector<>();
+        //初始化票据池
+        for(int i=0;i<1000;i++){
+            tickets.add("火车票" + i);
+        }
+         //10个窗口售票
+         for(int i=0;i<5;i++){
+             new Thread(){
+                    @Override
+                    public void run() {
+                         while(true){
+                            System.out.println(Thread.currentThread().getId() +"——"+ tickets.remove(0));
+                         }
+                    };
+            }.start();
+             /*new Thread(new Runnable() {
+                 @Override
+                 public void run() {
+                     while(true){
+                         System.out.println(Thread.currentThread().getId() +"——"+ tickets.remove(0));
+                     }
+                 }
+             }).start();*/
+         }
+
+    }
+}
